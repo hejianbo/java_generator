90fd8e1d081a2e56aa6e11a37bf35da716772cff
james <hejanbo@qq.com>
2019-04-05 15:39:58 +0800

    初始化文件结构

---
+++ config/basic_config.py
@@ -0,0 +1,9 @@
+base_package = "jmall.pms"
+
+dao_package = base_package + ".service.dao"
+dao_impl_package = base_package + ".service.dao.impl"
+mapper_package = base_package + ".service.dao.impl.mapper"
+entity_package = base_package + ".service.dao.entity"
+
+service_package = base_package + ".service"
+
