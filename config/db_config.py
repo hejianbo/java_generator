0bf57242d397f20cdd7678da59c99a0690c0338c
james.he <james.he@oyohotels.cn>
2019-08-09 14:50:56 +0800

    修改模板

--- config/db_config.py
+++ config/db_config.py
@@ -4,4 +4,4 @@ db_user = "t_manufacture_1"
 db_password = "fIch0y7rUQYS#6ARD^xW"
 database = "manufacture"
 # 以逗号分隔
-tables = "requirement_predict_history,requirement_predict"
+tables = "requirement_predict_history,replenish_predict_config_history,requirement_predict,requirement_predict,requirement_predict_item,predict_execute_config,replenishment_predict_config,predict_result"
